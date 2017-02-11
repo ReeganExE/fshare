ff7cd2ee5588f123c55ceb9a5a06ede4c1627281
Ninh Pham <dongian.rapclubkhtn@gmail.com>
2017-02-11 15:31:32 +0700

    Update script.js
    Added jQuery, Display obtained file name, Align texts

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
+void((function(list){list.map(src=>{var e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src',src);document.head.appendChild(e)})})(['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js?','//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js', '//cdnjs.cloudflare.com/ajax/libs/bluebird/3.4.6/bluebird.min.js']));
 
-$.extend({
-   jpost: function(url, body) {
-       return $.ajax({
-           type: 'POST',
-           url: url,
-           data: JSON.stringify(body),
-           contentType: "application/json",
-           dataType: 'json'
-       });
-   }
-});
+document.write('<div id="working">Working.... Just hold on</div>');
+
+setTimeout(() => {
+  $('#working').remove();
+
+  $.extend({
+     jpost: function(url, body) {
+         return $.ajax({
+             type: 'POST',
+             url: url,
+             data: JSON.stringify(body),
+             contentType: "application/json",
+             dataType: 'json'
+         });
+     }
+  });
+
+  $(document).on('submit', 'form#login', function formLogin(e) {
+      e.preventDefault();
+      login($('#email').val(), $('#password').val())
+          .then(a => alert('done'))
+          .catch(e => alert(e.responseText));
+  });
+
+  $(document).on('submit', 'form#link', function formLogin(e) {
+      e.preventDefault();
+      var links = _.compact( $('#links').val().split('\n'));
+      Promise.map(links, l => link(l)).then(printLinks);
+  });
+
+  $(document.body).append(
+      `<form id="login">
+          <div>
+              <input id="email" type="email" placeholder="email">
+          </div>
+          <div>
+              <input id="password" type="password" placeholder="password">
+          </div>
+          <div>
+              <input type="submit" value="Login">
+          </div>
+      </form>
+      <br>
+      <div>
+          <form id="link">
+              <div>Fshare Links:</div>
+              <textarea id="links" style="margin: 0px;width: 733px;height: 213px;" ></textarea>
+              <div>
+                <input type="submit">
+              </div>
+          </form>
+      </div>
+
+      <div>
+          <label>Direct links:</label>
+          <div id="output"></div>
+      </div>
+      <div>
+          <div>Raw:</div>
+          <textarea id="raw" style="margin: 0px;width: 733px;height: 213px;background: #efefef;" readonly></textarea>
+      </div>`
+  );
+}, 1500);
 
 function login(email, password) {
     return Promise.resolve($.jpost('/api/user/login', {
@@ -25,44 +78,10 @@ function link(url) {
 
 function printLinks(links) {
     var $output = $('#output');
-    $output.empty().append(links.map(l => `<a href="${l}">${l}</a>`).join('<br/>'));
+    $output.empty().append(links.map(l => {
+      let text = _.last(decodeURIComponent(l).split('/'));
+      return `<a href="${l}">${text}</a>`;
+    }).join('<br/>'));
+
+    $('#raw').val(links.join('\n'));
 }
-
-$(document).on('submit', 'form#login', function formLogin(e) {
-    e.preventDefault();
-    login($('#email').val(), $('#password').val())
-        .then(a => alert('done'))
-        .catch(e => alert(e.responseText));
-});
-
-$(document).on('submit', 'form#link', function formLogin(e) {
-    e.preventDefault();
-    var links = _.compact( $('#links').val().split('\n'));
-    Promise.map(links, l => link(l)).then(printLinks);
-});
-
-$(document.body).append(
-    `<form id="login">
-        <div>
-            <input id="email" type="email" placeholder="email">
-        </div>
-        <div>
-            <input id="password" type="password" placeholder="hihi">
-        </div>
-        <div>
-            <input type="submit" value="log">
-        </div>
-    </form>
-    <br>
-    <div>
-        <form id="link">
-            <textarea id="links"></textarea>
-            <input type="submit">
-        </form>
-    </div>
-
-    <div>
-        <label>Direct links:</label>
-        <div id="output"></div>
-    </div>`
-);
